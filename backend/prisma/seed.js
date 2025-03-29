54d5a8dff0e4db5a9f999e13bc4c38d93dd9f9db
badr2e <futxatpro@gmail.com>
2025-03-29 17:26:25 +0100

    feat: add tags and associated exercises to the seed data

--- backend/prisma/seed.js
+++ backend/prisma/seed.js
@@ -246,6 +246,269 @@ async function main() {
     },
   ];
   await prisma.activites.createMany({ data: activites });
+
+  // Ajout des tags
+  const tags = [
+    // Tags pour les exercices et programmes
+    { nom: "Cardio", type: "sport" },
+    { nom: "Force", type: "sport" },
+    { nom: "Flexibilité", type: "sport" },
+    { nom: "HIIT", type: "sport" },
+    { nom: "Poids du corps", type: "sport" },
+    { nom: "Haut du corps", type: "sport" },
+    { nom: "Bas du corps", type: "sport" },
+    { nom: "Core", type: "sport" },
+    { nom: "Équilibre", type: "sport" },
+    { nom: "Mobilité", type: "sport" },
+
+    // Tags pour les aliments
+    { nom: "Végétarien", type: "aliment" },
+    { nom: "Faible en glucides", type: "aliment" },
+    { nom: "Riche en protéines", type: "aliment" },
+    { nom: "Sans gluten", type: "aliment" },
+    { nom: "Petit-déjeuner", type: "aliment" },
+    { nom: "Collation", type: "aliment" },
+    { nom: "Dessert", type: "aliment" },
+    { nom: "Rapide", type: "aliment" },
+  ];
+  await prisma.tags.createMany({ data: tags });
+
+  // Récupération des IDs des tags pour les associer aux exercices
+  const tagIds = await prisma.tags.findMany({
+    where: { type: "sport" },
+    select: { id_tag: true, nom: true },
+  });
+
+  // Création des exercices avec leurs tags associés
+  const exercicesWithTags = [
+    {
+      exercice: {
+        nom: "Pompes classiques",
+        gif: "/assets/images/exercises/pushup.gif",
+        description:
+          "Position de planche, descendez en pliant les coudes puis remontez en les tendant.",
+        equipement: "Aucun",
+      },
+      tags: ["Force", "Poids du corps", "Haut du corps"],
+    },
+    {
+      exercice: {
+        nom: "Squats",
+        gif: "/assets/images/exercises/squat.gif",
+        description:
+          "Debout, pieds écartés largeur des épaules, descendez comme pour vous asseoir puis remontez.",
+        equipement: "Aucun",
+      },
+      tags: ["Force", "Poids du corps", "Bas du corps"],
+    },
+    {
+      exercice: {
+        nom: "Fentes avant",
+        gif: "/assets/images/exercises/lunge.gif",
+        description:
+          "Faites un grand pas en avant et pliez les genoux à 90 degrés, puis revenez en position initiale.",
+        equipement: "Aucun",
+      },
+      tags: ["Force", "Poids du corps", "Bas du corps"],
+    },
+    {
+      exercice: {
+        nom: "Planche",
+        gif: "/assets/images/exercises/plank.gif",
+        description:
+          "En appui sur les avant-bras et la pointe des pieds, maintenez votre corps droit comme une planche.",
+        equipement: "Aucun",
+      },
+      tags: ["Force", "Poids du corps", "Core"],
+    },
+    {
+      exercice: {
+        nom: "Burpees",
+        gif: "/assets/images/exercises/burpee.gif",
+        description:
+          "Enchaînez une position accroupie, une planche, une pompe, un retour accroupi et un saut vertical.",
+        equipement: "Aucun",
+      },
+      tags: ["Cardio", "HIIT", "Poids du corps"],
+    },
+    {
+      exercice: {
+        nom: "Mountain climbers",
+        gif: "/assets/images/exercises/montainclimber.gif",
+        description:
+          "En position de planche, ramenez alternativement les genoux vers la poitrine.",
+        equipement: "Aucun",
+      },
+      tags: ["Cardio", "HIIT", "Core"],
+    },
+    {
+      exercice: {
+        nom: "Jumping jacks",
+        gif: "/assets/images/exercises/jumpingjack.gif",
+        description:
+          "Sautez en écartant jambes et bras simultanément, puis revenez en position initiale.",
+        equipement: "Aucun",
+      },
+      tags: ["Cardio", "HIIT", "Poids du corps"],
+    },
+    {
+      exercice: {
+        nom: "Dips sur chaise",
+        gif: "/assets/images/exercises/dips.gif",
+        description:
+          "Mains sur le bord d'une chaise, fléchissez les coudes pour descendre puis remontez.",
+        equipement: "Chaise",
+      },
+      tags: ["Force", "Haut du corps"],
+    },
+    {
+      exercice: {
+        nom: "Curl biceps avec haltères",
+        gif: "/assets/images/exercises/bicepscurl.gif",
+        description:
+          "Coudes près du corps, fléchissez les avant-bras pour amener les haltères aux épaules.",
+        equipement: "Haltères",
+      },
+      tags: ["Force", "Haut du corps"],
+    },
+    {
+      exercice: {
+        nom: "Développé épaules avec haltères",
+        gif: "/assets/images/exercises/shoulderpress.gif",
+        description:
+          "Poussez les haltères au-dessus de la tête en tendant les bras, puis redescendez.",
+        equipement: "Haltères",
+      },
+      tags: ["Force", "Haut du corps"],
+    },
+    {
+      exercice: {
+        nom: "Rowing avec haltères",
+        gif: "/assets/images/exercises/rowing.gif",
+        description:
+          "Incliné en avant, tirez les haltères vers votre abdomen en serrant les omoplates.",
+        equipement: "Haltères",
+      },
+      tags: ["Force", "Haut du corps"],
+    },
+    {
+      exercice: {
+        nom: "Superman",
+        gif: "/assets/images/exercises/superman.gif",
+        description:
+          "Allongé sur le ventre, soulevez simultanément bras et jambes du sol.",
+        equipement: "Aucun",
+      },
+      tags: ["Force", "Core", "Poids du corps"],
+    },
+    {
+      exercice: {
+        nom: "Bicycle crunch",
+        gif: "/assets/images/exercises/crunchbicycle.gif",
+        description:
+          "Allongé sur le dos, amenez alternativement le coude vers le genou opposé.",
+        equipement: "Aucun",
+      },
+      tags: ["Core", "Poids du corps"],
+    },
+    {
+      exercice: {
+        nom: "Russian twist",
+        gif: "/assets/images/exercises/russiantwist.gif",
+        description:
+          "Assis avec le buste incliné en arrière, pivotez le torse d'un côté à l'autre.",
+        equipement: "Optionnel: poids léger",
+      },
+      tags: ["Force", "Core"],
+    },
+    {
+      exercice: {
+        nom: "Gainage latéral",
+        gif: "/assets/images/exercises/sideplank.gif",
+        description:
+          "En appui sur un avant-bras et la tranche du pied, maintenez le corps aligné.",
+        equipement: "Aucun",
+      },
+      tags: ["Force", "Core", "Équilibre"],
+    },
+    {
+      exercice: {
+        nom: "Extensions des triceps avec haltère",
+        gif: "/assets/images/exercises/tricepsextension.gif",
+        description:
+          "Bras au-dessus de la tête, fléchissez le coude pour descendre l'haltère derrière la tête.",
+        equipement: "Haltère",
+      },
+      tags: ["Force", "Haut du corps"],
+    },
+    {
+      exercice: {
+        nom: "Dead bug",
+        gif: "/assets/images/exercises/deadbug.gif",
+        description:
+          "Allongé sur le dos, étendez simultanément le bras droit et la jambe gauche.",
+        equipement: "Aucun",
+      },
+      tags: ["Core", "Mobilité", "Équilibre"],
+    },
+    {
+      exercice: {
+        nom: "Vélo",
+        gif: "/assets/images/exercises/bicycle.gif",
+        description:
+          "Allongé sur le dos, amenez alternativement le coude vers le genou opposé.",
+        equipement: "Aucun",
+      },
+      tags: ["Core", "Poids du corps"],
+    },
+    {
+      exercice: {
+        nom: "Bird dog",
+        gif: "/assets/images/exercises/birddog.gif",
+        description:
+          "À quatre pattes, étendez simultanément le bras droit et la jambe gauche.",
+        equipement: "Aucun",
+      },
+      tags: ["Core", "Équilibre", "Mobilité"],
+    },
+    {
+      exercice: {
+        nom: "Relevés de jambes suspendu",
+        gif: "/assets/images/exercises/legraises.gif",
+        description:
+          "Suspendu à une barre, remontez les jambes tendues jusqu'à l'horizontale.",
+        equipement: "Barre de traction",
+      },
+      tags: ["Force", "Core", "Poids du corps"],
+    },
+  ];
+
+  // Fonction pour trouver l'ID d'un tag par son nom
+  const findTagIdByName = (tagName) => {
+    const tag = tagIds.find((tag) => tag.nom === tagName);
+    return tag ? tag.id_tag : null;
+  };
+
+  // Insertion des exercices avec leurs tags associés
+  for (const item of exercicesWithTags) {
+    // Création de l'exercice
+    const createdExercice = await prisma.exercices.create({
+      data: item.exercice,
+    });
+
+    // Attribution des tags spécifiques à l'exercice
+    for (const tagName of item.tags) {
+      const tagId = findTagIdByName(tagName);
+      if (tagId) {
+        await prisma.exercices_tags.create({
+          data: {
+            id_exercice: createdExercice.id_exercice,
+            id_tag: tagId,
+          },
+        });
+      }
+    }
+  }
 }
 
 main()
